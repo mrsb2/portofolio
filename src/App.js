d5011a3d29d3aca7f28b3a63417f42432cb025b2
mrsomebody02 <iliecristian12@gmail.com>
2023-02-18 04:22:08 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import {About, Home, Show} from './pages';
 
 function App() {
   return (
     <div className="App">
-
+      <a href='https://colorhunt.co/palette/282f44e6af2ef5d061ececec'><h1>COLOR</h1></a>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as NavBar} from './navbar/Navbar';
+export {default as ShowContainer} from './showcontainer/ShowContainer';
+export {default as Contact} from './contact/Contact';
+export {default as Footer} from './footer/Footer';
